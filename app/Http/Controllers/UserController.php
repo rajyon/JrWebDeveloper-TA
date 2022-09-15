b1bcf86cb74cd9e8b1d96d216cc5dfc8041826eb
mipmorp4 <95652121+rajyon@users.noreply.github.com>
2022-09-15 16:19:15 +0800

    init

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 
 class UserController extends Controller
 {
+    // Create a POST API endpoint that accepts 3 parameters: name, email, ip_address.
     function addUser(Request $req){
 
         $user = new User;
@@ -21,10 +22,12 @@ class UserController extends Controller
         }
 
     }
+    // Create a GET API endpoint that returns all of the users with name, email, country and city fields.
     function getUsers(){
         $users = User::all(['name','email','country','city']);
         return $users;
     }
+    // Create a GET API endpoint that returns a user by ID with name, email, country and city fields.
     function getUser(Request $req){
         $user = User::find($req->id, ['name','email','country','city']);
         return $user;
